314abd3ba46f10dc71a0d9629441101ec9cd52b1
livius09 <levi.wimroither@hotmail.com>
2024-10-17 11:37:01 +0200

    Create 8-bit_stack.c

---
+++ 8-bit_stack.c
@@ -0,0 +1,84 @@
+#include <Wire.h>
+
+// EEPROM I2C address with A0 = 1 (A1 and A2 = 0, 0)
+#define EEPROM_ADDRESS 0x51  // Base address is 0x50, A0=1 adds 1
+
+// EEPROM memory address to write to (choose an address within the 64KB range)
+#define EEPROM_MEM_ADDR 0x0000
+
+uint16_t nextfree = 0x0000;
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+  Wire.begin();
+  
+
+  uint16_t numberToWrite = 12344;  // The number to write to EEPROM
+  bool wp = 0;
+  uint16_t ar[] = {1, 2, 3, 4, 5};  // Array of numbers to write
+  int len = sizeof(ar) / sizeof(ar[0]);
+
+  // Set write protect pin
+  digitalWrite(2, wp ? HIGH : LOW);
+
+  for(int i =0; i<len; i++){
+    push(ar[i]);
+    Serial.print("pushed: ");
+    Serial.println(ar[i]);
+  }
+  for(int j =0; j<len; j++){
+    Serial.print("poped: ");
+    Serial.println(pop());
+  }
+  
+}
+
+void loop() {
+  // Empty loop
+}
+
+void writeNumberToEEPROM(uint16_t memAddr, uint16_t number) {
+  Wire.beginTransmission(EEPROM_ADDRESS);
+
+  // Send the memory address (two bytes: high byte and low byte)
+  Wire.write((memAddr >> 8) & 0xFF);  // High byte
+  Wire.write(memAddr & 0xFF);         // Low byte
+
+  // Send the number (two bytes: high byte and low byte)
+  Wire.write(number);   //byte
+
+  Wire.endTransmission();
+  delay(10);  // Wait for EEPROM to complete the write process
+}
+
+uint16_t readNumberFromEEPROM(uint16_t memAddr) {
+  Wire.beginTransmission(EEPROM_ADDRESS);
+
+  // Send the memory address (two bytes: high byte and low byte)
+  Wire.write((memAddr >> 8) & 0xFF);  // High byte
+  Wire.write(memAddr & 0xFF);         // Low byte
+  Wire.endTransmission();
+
+  // Request 2 bytes of data from EEPROM
+  Wire.requestFrom(EEPROM_ADDRESS, 1);
+
+  int8_t number = 0;
+  if (Wire.available() >= 1) {
+    number = Wire.read();        // Read low byte
+  }
+
+  return number;
+}
+
+void push(int8_t in){
+  writeNumberToEEPROM( nextfree , in );
+  nextfree++;
+}
+
+int8_t pop(){
+  --nextfree;
+  return readNumberFromEEPROM(nextfree);
+}
+
+
