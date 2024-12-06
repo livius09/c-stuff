bb79df3e937eb492b5cd5c572d3573cc345fb0cc
livius09 <levi.wimroither@hotmail.com>
2024-12-06 22:50:41 +0100

    first working version

--- FCP/Read.c
+++ FCP/Read.c
@@ -1,10 +1,14 @@
 #include <Wire.h>
 
+// Forward declaration of the beep function
+void beep(uint8_t[]);
+
 // Declare function pointer array with 1 function (beep)
-void (*function_array[1])(char[]) = {beep};
-uint16 funcs = sizeof(function_array)/sizeof(function_array[0]);
+void (*function_array[])(uint8_t[]) = {beep};
 
 void setup() {
+  Serial.begin(9600);
+
   Wire.begin(0x01);  // Set up I2C address
   Wire.onReceive(receiveEvent);  // Set the receive event handler
   pinMode(4, OUTPUT);  // Set pin 4 as an output for the beep function
@@ -15,29 +19,40 @@ void loop() {
 }
 
 void receiveEvent(int bytes) {
-  char out[30];  // Buffer to store received data
-  char low = Wire.read();  // Read low byte
-  char high = Wire.read();  // Read high byte
+  uint8_t out[30];  // Buffer to store received data
+  uint8_t low = Wire.read();  // Read low byte
+  uint8_t high = Wire.read();  // Read high byte
   uint16_t func = ((uint16_t)high << 8) | low;  // Combine to get function ID
+  Serial.print("function: ");
+  Serial.println(func);
 
   // Ensure there are enough bytes available before reading into out
-  for (char i = 0; i < bytes - 2; i++) {
+  for (uint8_t i = 0; i < bytes - 2; i++) {
     if (Wire.available()) {
       out[i] = Wire.read();  // Read remaining bytes into out array
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(out[i]);
     }
   }
 
   // Ensure func is within bounds (0 in this case)
-  if (func < funcs) {
+  if (func < 1) {
     function_array[func](out);  // Call the function based on func index
   }
 }
 
-void beep(char onof[]) {
+void beep(uint8_t onof[]) {
   // Assuming the first byte indicates ON or OFF (HIGH or LOW)
-  if (onof[0] == '1') {
+  if (onof[0] == 1) {
     digitalWrite(4, HIGH);  // Turn on the beep (pin 4 HIGH)
+    Serial.println("beep on");
+    Serial.print("onof: ");
+    Serial.println(onof[0]);
   } else {
     digitalWrite(4, LOW);  // Turn off the beep (pin 4 LOW)
+    Serial.println("beep off");
+    Serial.print("onof: ");
+    Serial.println(onof[0]);
   }
 }
