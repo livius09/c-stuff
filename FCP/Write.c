46d86eb5491b092fd675530fac0ab8540b595a24
livius09 <levi.wimroither@hotmail.com>
2024-12-06 22:51:17 +0100

    first working version

--- FCP/Write.c
+++ FCP/Write.c
@@ -1,12 +1,20 @@
 #include <Wire.h>  // Correct header
 
+uint8_t args1[] = {0x01};  // Example byte array
+uint8_t args0[] = {0x00};
+
 void setup() {
   Wire.begin();  // Initialize I2C communication
+  Serial.begin(9600);
 }
 
 void loop() {
-  uint8_t args[] = {0x01};  // Example byte array
-  send_FCP(0x01, 0x01, args, sizeof(args));
+  
+  send_FCP(0x01, 0x00, args1, sizeof(args1));
+  delay(3000);
+  send_FCP(0x01, 0x00, args0, sizeof(args0));
+  delay(3000);
+
 }
 
 int send_FCP(uint8_t address, uint16_t function, const uint8_t* args, uint8_t length) {
@@ -19,10 +27,15 @@ int send_FCP(uint8_t address, uint16_t function, const uint8_t* args, uint8_t le
   // Write function ID as two bytes
   Wire.write(function & 0xFF);        // Low byte
   Wire.write((function >> 8) & 0xFF); // High byte
+  Serial.print("Function: ");
+  Serial.println(function);
 
   // Write each byte from the args array
   for (uint8_t i = 0; i < length; i++) {
     Wire.write(args[i]);
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(args[i]);
   }
 
   Wire.endTransmission();
